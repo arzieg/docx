a57c181c247b1d026cae594c557a92971e68210d
Arne Ziegler <arzieg@EZM306222.ez.edeka.net>
2024-08-16 22:05:20 +0200

    Deploy website - based on 582cfcd7a1433f804a1ed23dc0b0b111057408ca

---
+++ assets/js/31e63e32.b7f92c81.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocx=self.webpackChunkdocx||[]).push([[7357],{8822:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/docx/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
---
+++ assets/js/acecf23e.9936ed55.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocx=self.webpackChunkdocx||[]).push([[1903],{1912:o=>{o.exports=JSON.parse('{"blogBasePath":"/docx/blog","blogTitle":"Blog","authorsListPath":"/docx/blog/authors"}')}}]);
